5a75eebf2132bfc4dc9406bc38d1f880547d9f23
Marremeister <marius.westerlind@gmail.com>
2024-01-17 19:08:30 +0100

    added vehicle superclass

---
+++ Vehicle.java
@@ -0,0 +1,63 @@
+import java.awt.Color;
+
+public abstract class Vehicle {
+    protected int nrDoors; // Number of doors on the vehicle
+    protected double enginePower; // Engine power of the vehicle
+    protected double currentSpeed; // The current speed of the vehicle
+    protected Color color; // Color of the vehicle
+    protected String modelName; // The vehicle model name
+
+    public Vehicle(int nrDoors, double enginePower, Color color, String modelName) {
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+        stopEngine();
+    }
+
+    public int getNrDoors() {
+        return nrDoors;
+    }
+
+    public double getEnginePower() {
+        return enginePower;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color clr) {
+        color = clr;
+    }
+
+    public void startEngine() {
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine() {
+        currentSpeed = 0;
+    }
+
+    public abstract double speedFactor();
+
+    protected void incrementSpeed(double amount) {
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
+    }
+
+    protected void decrementSpeed(double amount) {
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
+    }
+
+    public void gas(double amount) {
+        incrementSpeed(amount);
+    }
+
+    public void brake(double amount) {
+        decrementSpeed(amount);
+    }
+}
